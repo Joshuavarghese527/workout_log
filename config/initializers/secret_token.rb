4e7acd6d7d6008e0bc99600679b2029ecfcc9126
Joshua Varghese <joshuavarghese527@gmail.com>
2016-05-19 04:57:01 +0000

    rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+WorkoutLog::Application.config.secret_key_base = 'dc7fb642703d00466f7fb951f8562f2754be76a6d591ab4f201d32e27c0f5b75f075ffe40c8c69d76fe648e18f4244ea7bc2fa71448f3f06e097f5e712587a99'
